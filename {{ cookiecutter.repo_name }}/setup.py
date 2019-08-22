048743933f127d0e1bfc81533fd99bf003e90935
Martí Bosch <marti.bosch@epfl.ch>
2019-08-22 10:36:15 +0200

    changed src dir for project_name; fix repo/project_name

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='{{ cookiecutter.repo_name }}',
+    packages=['{{ cookiecutter.project_name }}'],
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
